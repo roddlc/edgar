af7300211730de9d3169444014422a00c86636c3
Rodrigo de las Casas <roddlc90@gmail.com>
2021-11-28 21:41:12 -0500

    Outlining get_financial_statement function

--- bin/submission_text.py
+++ bin/submission_text.py
@@ -14,4 +14,25 @@ The aim of this script is to leverage BeautifulSoup to parse the .txt files made
 available by SEC to create clean dataframes for export.
 """
 
-# continue here
+# consider creating a class to process text data..
+
+def get_financial_statement(search_val,
+                            submission,
+                            user_agent,
+                            statement = None):
+
+    """
+    Return a dataframe with the data for the statement in question.
+
+    Args:
+        search_val (str): Company name (e.g., 'Microsoft Corporation') to search.
+        submission (str): Unique ID for the submission.
+        user_agent (str): Name and email to be included with data request to SEC 
+            API. This is required to submit the request. This field is formatted
+            'FirstName LastName email@domain'.
+        statement (str, default None): Name of the financial statement to return.
+            Accepts "Income Statement", "Balance Sheet", or "Cash Flow Statement".
+    
+    Returns:
+        dataframe: DataFrame containing financial statement data for a given submission.
+    """
