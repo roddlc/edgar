1ef843f1d4a2464e3bd18c287a6d53cf902c5804
Rodrigo de las Casas <roddlc90@gmail.com>
2021-11-28 22:23:35 -0500

    Building out basic control flow for get_financial_statement and adding a bit more to outline of that function

--- bin/submission_text.py
+++ bin/submission_text.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import os
 import utils
 import helper_functions as hf
+import requests
 
 
 """
@@ -31,8 +32,47 @@ def get_financial_statement(search_val,
             API. This is required to submit the request. This field is formatted
             'FirstName LastName email@domain'.
         statement (str, default None): Name of the financial statement to return.
-            Accepts "Income Statement", "Balance Sheet", or "Cash Flow Statement".
+            Accepts "Income Statement", "Balance Sheet", or "Cash Flow".
     
     Returns:
         dataframe: DataFrame containing financial statement data for a given submission.
     """
+
+    xml = hf.get_financial_report_metadata(search_val = search_val,
+                                           submission = submission,
+                                           user_agent = user_agent)
+    
+    # depending on statement arg passed, select the report_url for that statement
+    # e.g., if "Income Statement", select report_url for https://www.sec.gov/Archives/edgar/data/789019/000156459021051992/R2.htm
+
+    if statement == "Income Statement":
+
+        url = xml.query('report_short_name == "INCOME STATEMENTS"')['report_url'].tolist()[0]
+        print(url)
+
+        header = {'User-Agent': user_agent}
+        r = requests.get(url, headers = header)
+
+    #print(r.content)
+
+    elif statement == "Balance Sheet": #! consider replacing == with list of acceptable values for a .contains + casing = False
+
+        # <code here>
+        print('this is a placeholder')
+
+    elif statement == "Cash Flow":
+
+        # <code here>
+        print('this is a placeholder')
+    
+    else:
+
+        print("Error: You did not provide an acceptable value. Try 'Income Statement', 'Balance Sheet', or 'Cash Flow'")
+
+    # having received web content, pivot to beautiful soup
+
+    soup = BeautifulSoup(r, 'html')
+
+    # parse soup and create dataframe
+
+    # more here..
